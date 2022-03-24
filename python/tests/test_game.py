54dc1fae5af357094884fd658e55402b0b751b19
Kyle Adams <kadams54@users.noreply.github.com>
2022-03-24 10:51:37 -0400

    Py: Simplify the initial game state assertions

--- python/tests/test_game.py
+++ python/tests/test_game.py
@@ -1,7 +1,5 @@
 from tennis_scoring.game import (start, Player, Score)
 
 def test_start():
-    game = start()
-    assert game[Player.ONE] == Score.LOVE
-    assert game[Player.TWO] == Score.LOVE
-    assert game['winner'] == None
+    assert start() == {
+        Player.ONE: Score.LOVE, Player.TWO: Score.LOVE, 'winner': None}
