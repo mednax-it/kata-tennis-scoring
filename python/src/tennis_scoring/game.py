07724e2e7a278497c7917eb1c9c1db2f30ba320d
Kyle Adams <kadams54@users.noreply.github.com>
2022-03-24 09:27:48 -0400

    Add winner to game state

--- python/src/tennis_scoring/game.py
+++ python/src/tennis_scoring/game.py
@@ -1,2 +1,16 @@
+from enum import Enum
+
+class Player(Enum):
+    ONE = 1
+    TWO = 2
+
+
+class Score(Enum):
+    LOVE = 0
+    FIFTEEN = 15
+    THIRTY = 30
+    FOURTY = 40
+
+
 def start():
-    return ({'score': 0}, {'score': 0})
+    return {Player.ONE: Score.LOVE, Player.TWO: Score.LOVE, 'winner': None}
