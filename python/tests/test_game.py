f237a87c07b1048985bab4243de991ec25519946
Kyle Adams <kadams54@users.noreply.github.com>
2022-03-28 09:29:16 -0400

    Py: Failing test for incrementing the score

--- python/tests/test_game.py
+++ python/tests/test_game.py
@@ -1,5 +1,15 @@
-from tennis_scoring.game import (start, Player, Score)
+import pytest
+
+from tennis_scoring.game import (next_score, start, Player, Score)
 
 def test_start():
     assert start() == {
         Player.ONE: Score.LOVE, Player.TWO: Score.LOVE, 'winner': None}
+
+
+def test_next_score():
+    assert next_score(Score.LOVE) == Score.FIFTEEN
+    assert next_score(Score.FIFTEEN) == Score.THIRTY
+    assert next_score(Score.THIRTY) == Score.FOURTY
+    with pytest.raises(IndexError):
+        next_score(Score.FOURTY)
