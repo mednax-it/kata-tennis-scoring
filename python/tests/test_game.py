846967772b1671aee2450dffba2d052fd088dcae
Kyle Adams <kadams54@users.noreply.github.com>
2022-03-28 13:44:45 -0400

    Support winning state when updating score

--- python/tests/test_game.py
+++ python/tests/test_game.py
@@ -11,5 +11,5 @@ def test_next_score():
     assert next_score(Score.LOVE) == Score.FIFTEEN
     assert next_score(Score.FIFTEEN) == Score.THIRTY
     assert next_score(Score.THIRTY) == Score.FOURTY
-    with pytest.raises(IndexError):
-        next_score(Score.FOURTY)
+    assert next_score(Score.FOURTY) == Score.WINNER
+    assert next_score(Score.WINNER) == None
