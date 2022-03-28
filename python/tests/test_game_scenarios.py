656410ef184456148c332fdaa2c5f5bc85586145
Kyle Adams <kadams54@users.noreply.github.com>
2022-03-28 16:53:29 -0400

    Move logic updating game state into point function

--- python/tests/test_game_scenarios.py
+++ python/tests/test_game_scenarios.py
@@ -1,14 +1,7 @@
-import random
-
-from tennis_scoring.game import (next_score, start, Player, Score)
+from tennis_scoring.game import (point, start, Player, Score)
 
 def test_non_tied_game():
-    my_game = start()
-    while not my_game['winner']:
-        scorer = Player.ONE
-        current_score = my_game[scorer]
-        my_game[scorer] = next_score(current_score)
-        if my_game[scorer] == Score.WINNER:
-            my_game['winner'] = scorer
-    assert my_game['winner'] != None
-    assert my_game['winner'] == Player.ONE
+    game = start()
+    while not game['winner']:
+        game = point(game, Player.ONE)
+    assert game['winner'] == Player.ONE
