fa8974bf184315cb7cbaba5dee298aa6ab7274fc
Kyle Adams <kadams54@users.noreply.github.com>
2022-04-11 10:05:28 -0400

    TS: Failing test for iter 2: advance the score

--- typescript/test/game.test.ts
+++ typescript/test/game.test.ts
@@ -9,4 +9,13 @@ describe("game", () => {
       expect(winner).toBe(null);
     });
   });
+
+  describe("#nextScore", () => {
+    it("should advance a player's score to the next point value", () => {
+      expect(nextScore(Score.LOVE)).toBe(Score.FIFTEEN);
+      expect(nextScore(Score.FIFTEEN)).toBe(Score.THIRTY);
+      expect(nextScore(Score.THIRTY)).toBe(Score.FOURTY);
+      expect(nextScore(Score.FOURTY)).toBe(Score.WINNER);
+    });
+  });
 });
