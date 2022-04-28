57283ec575a4d4b7bcbcb8d026b2e60b63ce082c
Kyle Adams <kadams54@users.noreply.github.com>
2022-04-28 09:04:00 -0400

    Py: Black formatting

--- python/src/tennis_scoring/game.py
+++ python/src/tennis_scoring/game.py
@@ -1,5 +1,6 @@
 from enum import Enum
 
+
 class Player(Enum):
     ONE = 1
     TWO = 2
@@ -15,12 +16,13 @@ class Score(Enum):
 
 def start():
     return {
-        'scores': {
+        "scores": {
             Player.ONE: Score.LOVE,
             Player.TWO: Score.LOVE,
         },
-        'winner': None,
+        "winner": None,
     }
+
 
 def next_score(current_score):
     if current_score == Score.WINNER:
@@ -28,14 +30,12 @@ def next_score(current_score):
     scores = list(Score)
     return scores[scores.index(current_score) + 1]
 
+
 def award_point(game, player):
-    new_score = next_score(game['scores'][player])
+    new_score = next_score(game["scores"][player])
     new_scores = {
-        **game['scores'],
-        **{ player: new_score },
+        **game["scores"],
+        **{player: new_score},
     }
     winner = player if new_score == Score.WINNER else None
-    return {
-        'scores': scores,
-        'winner': winner
-    }
+    return {"scores": scores, "winner": winner}
